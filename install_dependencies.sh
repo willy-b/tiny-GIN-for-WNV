64d2bae77751f205342dd2f77c1b0b841c689546
William Bruns <adde.animulis@gmail.com>
2025-05-02 16:58:34 -0400

    ogb toolkit we depend on is not yet compatible with pytorch 2.6 or later so ensure it uses older version by pinning

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+pip install torch==2.5.1
 pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-2.2.1+cu121.html
 pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-2.2.1+cu121.html
-pip install torch-geometric
-pip install ogb
+pip install torch-geometric==2.5.3
+pip install ogb==1.3.6
 pip install rdkit
 pip install scikit-learn>=1.3
 pip install deepchem==2.8.0 # only used for scaffold splitting
